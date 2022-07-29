fc9366f000d87b46e4fa0fc356e5c7847e555593
moronesia <moronesia8@gmail.com>
2022-07-29 21:19:35 +0700

    update protfolio react bootstrap

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-// import App from './App';
-import Bootstrap from "../src/Bootstrap";
-
-test('renders learn react link', () => {
-  render(<Bootstrap />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
